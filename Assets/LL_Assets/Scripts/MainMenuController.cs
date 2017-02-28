2af5ed8419fd2539266ba7d4ccb029ee662559aa
meaganbudgell <meagan.budgell@gmail.com>
2017-02-28 17:16:31 -0500

    Kinect menu in!! , waiting on functionality

---
+++ Assets/LL_Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,217 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour, IDragHandler {
+
+    public enum MenuState { Intro, SongSelect, BGSelect };
+    [SerializeField] MenuState currentState;
+    [SerializeField] MenuState lastState;
+    [SerializeField] RectTransform menuHolder;
+
+    Vector3 newPos = Vector3.zero;
+    Vector3 menuVelocity = Vector3.zero;
+
+    bool moveUI = false;
+    float destXPos;
+    float smoothTime = 0.5f;
+    float start = 0;
+    float end = 0;
+    int dir = 0;
+
+	// Use this for initialization
+	void Start () {
+        // Debug.Log("Hi");
+        currentState = MenuState.Intro;
+        lastState = MenuState.Intro;
+	}
+
+    void Update() {
+        MoveUI();
+
+        /*int fingerCount = 0;
+        foreach (Touch touch in Input.touches) {
+            if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+                fingerCount++;
+        }
+
+        ProcessInputs();*/
+    }
+
+    void UpdateMenuState(bool moveRight) {
+        // Debug.Log("Hit Space");
+        switch (currentState) {
+            case MenuState.Intro:
+                newPos = new Vector3(-1920, 0, 0);
+                //lastState = MenuState.Intro;
+                //currentState = MenuState.SongSelect;
+                break;
+            case MenuState.SongSelect:
+                if (lastState == MenuState.BGSelect) {
+                    newPos = new Vector3(-3840, 0, 0);
+                    //lastState = MenuState.Intro;
+                    //currentState = MenuState.BGSelect;
+                } else {
+                    newPos = new Vector3(0, 0, 0);
+                    //lastState = MenuState.Intro;
+                    //currentState = MenuState.Intro;
+                }
+                break;
+            case MenuState.BGSelect:
+                newPos = new Vector3(-1920, 0, 0);
+                //lastState = MenuState.BGSelect;
+                //currentState = MenuState.SongSelect;
+                break;
+        }
+        moveUI = true;
+    }
+
+    void MoveUI() {
+        menuHolder.localPosition = Vector3.SmoothDamp(menuHolder.localPosition, newPos, ref menuVelocity, smoothTime);
+    }
+
+    void RotateCamera() {
+        bool moveRight = false;
+
+        switch (Input.GetTouch(0).phase) {
+            case TouchPhase.Began:
+                // Debug.Log("Touch started at" + Input.GetTouch(0).deltaPosition.x);
+                start = Input.GetTouch(0).deltaPosition.x;
+                break;
+            case TouchPhase.Moved:
+                // Get the change in direction input
+                float xDir = Input.GetTouch(0).deltaPosition.x;
+                //Debug.Log("deltaPosition: " + Input.GetTouch(0).deltaPosition)
+                // Disregard any unintentional, tiny discrepencies in a swipe
+                if (Mathf.Abs(xDir) < 50) {
+                    start = 0;
+                    end = 0;
+                }
+                break;
+            case TouchPhase.Ended:
+                // Debug.Log("Touch Ended at: " + Input.GetTouch(0).deltaPosition.x);
+                end = Input.GetTouch(0).deltaPosition.x;
+                break;
+            }
+
+        if (start > end) {
+            Debug.Log("Move Left");
+            moveRight = false;
+        } else if (start < end) {
+            Debug.Log("Move Right");
+            moveRight = true;
+        }
+
+        UpdateMenuState(moveRight);
+
+        start = 0;
+        end = 0;
+    }
+
+    void ProcessInputs() {
+        if (Input.touchCount == 1) {
+            RotateCamera();
+        }
+    }
+
+    public void OnDrag(PointerEventData baseData) {
+
+        // Convert base data to pointer data to get position changes
+        PointerEventData pointerData = baseData;
+
+        RotateCamera(/*pointerData.delta.x*/);
+    }
+
+    public void ButtonClick(int val)
+    {
+        lastState = currentState;
+
+        switch (val)
+        {
+            case 0:
+                {
+                    currentState = MenuState.Intro;
+                    UpdateMenuState(true);
+                }
+                break;
+            case 1:
+                {
+                    currentState = MenuState.BGSelect;
+                    UpdateMenuState(true);
+                }
+                break;
+            case 2:
+                {
+                    currentState = MenuState.SongSelect;
+
+                    int bgToSave = 0;
+
+                    switch (EventSystem.current.currentSelectedGameObject.name)
+                    {
+                        case "Forest":
+                            {
+                                bgToSave = 2;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+
+                    PlayerPrefs.SetInt("bgNumber", bgToSave);
+                    PlayerPrefs.Save();
+
+                    UpdateMenuState(true);
+                }
+                break;
+            case 3:
+                {
+                    int sceneToSave = 0;
+
+                    switch (EventSystem.current.currentSelectedGameObject.name)
+                    {
+                        case "Seasons Change":
+                            {
+                                sceneToSave = 0;
+                            }
+                            break;
+                        case "Get Free":
+                            {
+                                sceneToSave = 1;
+                            }
+                            break;
+                        case "Dream Giver":
+                            {
+                                sceneToSave = 2;
+                            }
+                            break;
+                        case "Spirit Seeker":
+                            {
+                                sceneToSave = 3;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+
+                    PlayerPrefs.SetInt("sceneNumber", sceneToSave);
+                    PlayerPrefs.Save();
+
+                    // need to make scenes with the background and music in it already
+                    // for now just loads and finds the music/bg and sets it
+                    // since it is using ints, maybe have the scene named like Main_Mobile_01 for a backyard bg and Get Free music
+
+                    //string sceneName = "Main_Mobile_" + PlayerPrefs.GetInt("bgNumber").ToString() + PlayerPrefs.GetInt("sceneNumber").ToString();
+
+                    //SceneManager.LoadScene(sceneName);
+
+                    SceneManager.LoadScene("Main_Mobile");
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
--- Assets/Scripts/MenuFly.cs
+++ Assets/Scripts/MenuFly.cs
@@ -6,38 +6,52 @@ using UnityEngine.UI;
 public class MenuFly : MonoBehaviour {
 
 	public float glowlevel = 0.0f;
+    public float glowlevel2 = 0.0f;
 	public float playtime = 3;
 	public float wait = 2.0f;
 	[SerializeField] Text countdown;
-	// Use this for initialization
+    Color tempcolor2;
+    Image obj;
+    bool glowing = false;
+    // Use this for initialization
 
-	void OnTriggerStay2D(Collider2D col)
+   
+
+    void OnTriggerStay2D(Collider2D col)
 	{
-		if (col.gameObject.tag == "Play")
-		{
-			Debug.Log ("Glowing");
-			Glow ();
-		}
-		else
-			glowlevel = Mathf.Lerp(glowlevel,0.0f, Time.deltaTime);	
-	}
+        if (col.gameObject.CompareTag ("Button"))
+        {
+            Debug.Log("Glowing");
+            glowing = true;
+            Glow(col.gameObject);
+             obj = col.gameObject.GetComponent<Image>();
+        }
+        else
+        {
+            glowlevel = Mathf.Lerp(glowlevel, 0.0f, Time.deltaTime);
+            tempcolor2 = col.gameObject.GetComponent<Image>().color;
+        }
+    }
 
 	void OnTriggerExit2D(Collider2D col)
 	{
-		playtime = 3;
+        
+        playtime = 3;
 		countdown.gameObject.SetActive(false);
-
-	}
+        glowlevel = Mathf.Lerp(glowlevel, 0.0f, Time.deltaTime);
+    }
 	// Update is called once per frame
 	void Update () {
-
-
-		Color tempColor = this.GetComponent<SpriteRenderer> ().color;
+        
+        Color tempColor = this.GetComponent<SpriteRenderer> ().color;
 		tempColor.a = glowlevel;
 		if (Input.GetKey ("up")) {
-			glowlevel = Mathf.Lerp (glowlevel, 1.0f, Time.deltaTime);
-		} else {
-			glowlevel = Mathf.Lerp(glowlevel,0.0f, Time.deltaTime);	
+           // glowing = true;
+            glowlevel2 = Mathf.Lerp(glowlevel, 1.0f, Time.deltaTime);
+            glowlevel = Mathf.Lerp(glowlevel, 1.0f, Time.deltaTime);
+        } else {
+			glowlevel = Mathf.Lerp(glowlevel,0.0f, Time.deltaTime);
+            glowing = false;
 		}
 
 		if (Input.GetKeyDown ("down")) 
@@ -46,14 +60,26 @@ public class MenuFly : MonoBehaviour {
 		}
 
 		this.GetComponent<SpriteRenderer> ().color = tempColor;
-}
+        if (obj == null)
+        {
+            return;
+        }
+        obj.color = tempColor;
 
-	void Glow(){
-		//glowlevel += 0.5f;
-		glowlevel = Mathf.Lerp(glowlevel,1.0f, Time.deltaTime);
-		wait -= Time.deltaTime;
+       
+    }
 
-		if (wait <= 0) 
+	void Glow(GameObject col)
+    {
+        //tempcolor2.a = glowlevel2;
+
+        //glowlevel += 0.5f;
+        glowlevel = Mathf.Lerp(glowlevel, 1.0f, Time.deltaTime);
+            wait -= Time.deltaTime;
+            glowlevel2 = Mathf.Lerp(glowlevel2, 1.0f, Time.deltaTime);
+            wait -= Time.deltaTime;
+        
+        if (wait <= 0) 
 		{
 			countdown.gameObject.SetActive(true);
 			playtime -=Time.deltaTime;
